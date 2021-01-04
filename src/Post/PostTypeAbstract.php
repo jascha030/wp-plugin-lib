9ce17b0e9412291fa1e7c6a5e8f7ce61b9d1eddc
Jascha van Aalst <mr.jascha@gmail.com>
2021-01-04 17:53:32 +0100

    Added abstract with register method for post types

---
+++ src/Post/PostTypeAbstract.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Jascha030\PluginLib\Post;
+
+use Jascha030\PluginLib\Hookable\HookableAbstract;
+
+/**
+ * Class PostTypeAbstract
+ *
+ * @package Jascha030\PluginLib\Post
+ */
+abstract class PostTypeAbstract extends HookableAbstract
+{
+    /**
+     * @var string The post type slug
+     */
+    protected $slug;
+
+    /**
+     * @var array Arguments for register_post_type method
+     */
+    protected $arguments;
+
+    /**
+     * Register post type
+     *
+     * @return void
+     */
+    final public function register(): void
+    {
+        \register_post_type($this->slug, $this->arguments);
+    }
+
+    /**
+     * Hook register_post_type upon construction
+     *
+     * @return void
+     */
+    public function hookMethods(): void
+    {
+        \add_action('init', [$this, 'register']);
+    }
+}
