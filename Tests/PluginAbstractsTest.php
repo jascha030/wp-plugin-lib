66395feaf684d53e699d86114417ef404afa90e8
Jascha van Aalst <mr.jascha@gmail.com>
2021-01-04 17:45:16 +0100

    Test for abstract class

---
+++ Tests/PluginAbstractsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Jascha030\PluginLib\Plugin\WordpressPluginAbstract;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PluginAbstractsTest
+ */
+class PluginAbstractsTest extends TestCase
+{
+    protected $anonymousPluginClass;
+
+    public function testAbstractClassMethod(): void
+    {
+        self::assertInstanceOf(
+            WordpressPluginAbstract::class,
+            $this->anonymousPluginClass->getThis()
+        );
+    }
+
+    protected function setUp(): void
+    {
+        $this->anonymousPluginClass = new class extends WordpressPluginAbstract {
+            public function hookMethods(): void
+            {
+            }
+
+            public function getThis(): self
+            {
+                return $this;
+            }
+        };
+    }
+}
