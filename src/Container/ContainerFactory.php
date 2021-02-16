c535f7eabadb4a388b2788cf01f82c91b9eb4c7e
Jascha van Aalst <mr.jascha@gmail.com>
2021-02-16 02:14:15 +0100

    docs on singleton trait

--- src/Container/ContainerFactory.php
+++ src/Container/ContainerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Jascha030\PluginLib\Container;
 
+use Jascha030\PluginLib\Manager\Filter\FilterManager;
 use Pimple\Container;
 use Pimple\Psr11\Container as Psr11Container;
 use Pimple\ServiceProviderInterface;
@@ -14,15 +15,24 @@ class ContainerFactory
 {
     /**
      * @param  array|ServiceProviderInterface[]  $serviceProviders
+     * @param  array  $hookableServices
      * @return Psr11Container
      */
-    public function __invoke(array $serviceProviders = []): Psr11Container
+    public function __invoke(array $serviceProviders = [], array $hookableServices = []): Psr11Container
     {
         $container = new Container();
 
         foreach ($serviceProviders as $provider) {
             $provider->register($container);
         }
+
+        $container['filters'] = function (Container $container) use ($hookableServices) {
+            $manager = new FilterManager($container);
+
+            foreach ($hookableServices as $alias => $service) {
+                $manager->registerHookable($alias, $service);
+            }
+        };
 
         return new Psr11Container($container);
     }
