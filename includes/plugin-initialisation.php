e3c6dde7be9a14c7389044495e62527ec572374d
Jascha van Aalst <mr.jascha@gmail.com>
2021-02-21 17:45:57 +0100

    buildPlugin function check

--- includes/plugin-initialisation.php
+++ includes/plugin-initialisation.php
@@ -10,38 +10,43 @@ use Jascha030\PluginLib\Exception\DoesNotImplementInterfaceException;
 use Jascha030\PluginLib\Exception\Psr11\DoesNotImplementProviderInterfaceException;
 use Jascha030\PluginLib\Exception\Psr11\DoesNotImplementHookableInterfaceException;
 
-/**
- * Create's your plugin or theme class and injects all of it's dependencies
- *
- * @param  Config  $config Config object containing your Hookable classes, Service Providers and Post types
- * @param  string  $pluginClass Class name of your main plugin or theme class
- * @param  string  $file Main plugin or theme file (using this in that file and passing __FILE__ is recommended)
- * @param  string  $builderClass Custom builder class if you want to use a different Container than Pimple
- *
- * @throws DoesNotImplementInterfaceException
- * @throws DoesNotImplementHookableInterfaceException
- * @throws DoesNotImplementProviderInterfaceException
- *
- * @return FilterManagerInterface
- */
-function buildPlugin(Config $config, string $pluginClass, string $file, string $builderClass = Builder::class): FilterManagerInterface
-{
-    $config = (new Config('Test package plugin', __FILE__))->setHookables([
-        TestingHookable::class,
-        TestingAfterInitHookable::class
-    ]);
+if (! function_exists('buildPlugin')) {
+    /**
+     * Create's your plugin or theme class and injects all of it's dependencies
+     *
+     * @param  Config  $config        Config object containing your Hookable classes, Service Providers and Post types
+     * @param  string  $pluginClass   Class name of your main plugin or theme class
+     * @param  string  $file          Main plugin or theme file (using this in that file and passing __FILE__ is recommended)
+     * @param  string  $builderClass  Custom builder class if you want to use a different Container than Pimple
+     *
+     * @return FilterManagerInterface
+     * @throws DoesNotImplementHookableInterfaceException
+     * @throws DoesNotImplementProviderInterfaceException
+     *
+     * @throws DoesNotImplementInterfaceException
+     */
+    function buildPlugin(
+        Config $config,
+        string $pluginClass,
+        string $file,
+        string $builderClass = Builder::class
+    ): FilterManagerInterface {
+        $config = (new Config('Test package plugin', __FILE__))->setHookables([
+            TestingHookable::class, TestingAfterInitHookable::class
+        ]);
 
-    $container = (new $builderClass())($config);
+        $container = (new $builderClass())($config);
 
-    if (isset($container)) {
-        if (!is_subclass_of(FilterManagerInterface::class,pluginClass)) {
-            throw new DoesNotImplementInterfaceException($pluginClass, PluginApiRegistryAbstract::class);
+        if (isset($container)) {
+            if (! is_subclass_of(FilterManagerInterface::class, pluginClass)) {
+                throw new DoesNotImplementInterfaceException($pluginClass, PluginApiRegistryAbstract::class);
+            }
+
+            $plugin = new $pluginClass($container->get('hookable.reference'),
+                $container->get('hookable.afterInit'),
+                $container->get('hookable.locator'));
+
+            return $plugin;
         }
-
-        $plugin = new $pluginClass($container->get('hookable.reference'),
-            $container->get('hookable.afterInit'),
-            $container->get('hookable.locator'));
-
-        return $plugin;
     }
 }
