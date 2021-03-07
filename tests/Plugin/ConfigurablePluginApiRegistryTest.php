fe9b72b9eceb390fdd56b91aef36bd0ed6e2a92b
Jascha van Aalst <mr.jascha@gmail.com>
2021-03-07 21:39:52 +0100

    Started ConfigurablePlugin tests

--- tests/Plugin/ConfigurablePluginApiRegistryTest.php
+++ tests/Plugin/ConfigurablePluginApiRegistryTest.php
@@ -5,17 +5,18 @@ namespace Jascha030\Tests\Plugin;
 use Jascha030\PluginLib\Exception\Psr11\DoesNotImplementHookableInterfaceException;
 use Jascha030\PluginLib\Exception\Psr11\DoesNotImplementProviderInterfaceException;
 use Jascha030\PluginLib\Plugin\ConfigurablePluginApiRegistry;
+use Jascha030\PluginLib\Plugin\PluginApiRegistryInterface;
 use PHPUnit\Framework\TestCase;
 
-class ConfigurablePluginApiRegistryTest extends TestCase
+final class ConfigurablePluginApiRegistryTest extends TestCase
 {
     /**
      * @throws DoesNotImplementHookableInterfaceException
      * @throws DoesNotImplementProviderInterfaceException
      */
-    public function test__construct()
+    public function testConstruct(): void
     {
         $plugin = new ConfigurablePluginApiRegistry('test', dirname(__FILE__, 2));
-//        self::assertInstanceOf($plugin, Plugin)
+        self::assertInstanceOf($plugin, PluginApiRegistryInterface::class);
     }
 }
