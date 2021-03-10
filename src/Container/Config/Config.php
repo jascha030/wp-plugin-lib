516a586ce22779d51787a43be58f5c0512b81371
Jascha van Aalst <mr.jascha@gmail.com>
2021-03-10 23:31:40 +0100

    Remove redundant comments

--- src/Container/Config/Config.php
+++ src/Container/Config/Config.php
@@ -2,41 +2,35 @@
 
 namespace Jascha030\PluginLib\Container\Config;
 
+use Jascha030\PluginLib\Entity\Post\PostTypeInterface;
+use Jascha030\PluginLib\Exception\Psr11\DoesNotImplementHookableInterfaceException;
+use Jascha030\PluginLib\Exception\Psr11\DoesNotImplementProviderInterfaceException;
+use Jascha030\PluginLib\Service\Hookable\HookableAfterInitInterface;
+use Jascha030\PluginLib\Service\Hookable\HookableInterface;
+use Jascha030\PluginLib\Service\Hookable\LazyHookableInterface;
+use Jascha030\PluginLib\Service\Provider\WordpressProvider;
+use Pimple\Container;
+use Pimple\Container as PimpleContainer;
+use Pimple\Psr11\ServiceLocator;
+use Pimple\ServiceProviderInterface;
+
 /**
  * Class Config
  *
  * @package Jascha030\PluginLib\Container\Config
  */
-class Config
+final class Config implements ConfigInterface
 {
-    /**
-     * @var string
-     */
     private string $pluginName;
 
-    /**
-     * @var string
-     */
     private string $pluginFile;
 
-    /**
-     * @var array
-     */
     private array $postTypes = [];
 
-    /**
-     * @var array
-     */
     private array $hookables = [];
 
-    /**
-     * @var array
-     */
     private array $serviceProviders = [];
 
-    /**
-     * @var string
-     */
     private string $pluginPrefix;
 
     /**
@@ -67,7 +61,7 @@ class Config
      *
      * @return Config
      */
-    final public function setPostTypes(array $postTypes): self
+    public function setPostTypes(array $postTypes): self
     {
         $this->postTypes = $postTypes;
 
@@ -77,7 +71,7 @@ class Config
     /**
      * @return array|string[]
      */
-    final public function getHookables(): array
+    public function getHookables(): array
     {
         return $this->hookables;
     }
@@ -87,7 +81,7 @@ class Config
      *
      * @return Config
      */
-    final public function setHookables(array $hookables): self
+    public function setHookables(array $hookables): self
     {
         $this->hookables = $hookables;
 
@@ -97,7 +91,7 @@ class Config
     /**
      * @return array
      */
-    final public function getServiceProviders(): array
+    public function getServiceProviders(): array
     {
         return $this->serviceProviders;
     }
@@ -107,7 +101,7 @@ class Config
      *
      * @return Config
      */
-    final public function setServiceProviders(array $serviceProviders): self
+    public function setServiceProviders(array $serviceProviders): self
     {
         $this->serviceProviders = $serviceProviders;
 
@@ -117,7 +111,7 @@ class Config
     /**
      * @return string
      */
-    final public function getPluginName(): string
+    public function getPluginName(): string
     {
         return $this->pluginName;
     }
@@ -125,7 +119,7 @@ class Config
     /**
      * @return string
      */
-    final public function getPluginFile(): string
+    public function getPluginFile(): string
     {
         return $this->pluginFile;
     }
@@ -133,8 +127,102 @@ class Config
     /**
      * @return string
      */
-    final public function getPluginPrefix(): string
+    public function getPluginPrefix(): string
     {
         return $this->pluginPrefix;
+    }
+
+    /**
+     * @param  PimpleContainer  $container
+     *
+     * @throws DoesNotImplementHookableInterfaceException
+     * @throws DoesNotImplementProviderInterfaceException
+     */
+    public function configure(PimpleContainer $container): void
+    {
+        $hookables = $this->getHookables();
+        $postTypes = $this->getPostTypes();
+
+        $this->injectServiceProviders($container);
+
+        /**
+         * HookableClasses should strictly contain public methods to be hooked to wordpress actions/filters
+         * Other methods should be private/protected methods that are used by classes public methods.
+         * Globally available methods belong in a service that is provided by a ServiceProvider.
+         */
+        $afterInitHookables = [];
+        $reference          = [];
+
+        if (! empty($hookables)) {
+            foreach ($hookables as $className) {
+                if (! is_subclass_of($className, HookableInterface::class)) {
+                    throw new DoesNotImplementHookableInterfaceException($className);
+                }
+
+                if (is_subclass_of($className, LazyHookableInterface::class)) {
+                    $reference[$className] = [];
+                }
+
+                if (is_subclass_of($className, HookableAfterInitInterface::class)) {
+                    $afterInitHookables[] = $className;
+                }
+
+                $container[$className] = static function () use ($className) {
+                    return new $className();
+                };
+            }
+        }
+
+        $postTypesArray = [];
+
+        foreach ($postTypes as $postType) {
+            if (is_array($postType)) {
+                $postTypesArray[$postType[0]] = $postType;
+            }
+
+            if (is_subclass_of($postType, PostTypeInterface::class)) {
+                $postTypesArray[$postType] = $postType;
+            }
+        }
+
+        $container['hookable.reference'] = $reference;
+        $container['hookable.afterInit'] = $afterInitHookables;
+        $container['plugin.postTypes']   = $postTypesArray;
+
+        $container['hookable.locator'] = static function (Container $container) {
+            $lazyHookables    = array_keys($container['hookable.reference']);
+            $hookableServices = array_merge(
+                $lazyHookables,
+                $container['hookable.afterInit'],
+                $container['plugin.postTypes']
+            );
+
+            return new ServiceLocator($container, $hookableServices);
+        };
+    }
+
+    private function injectServiceProviders(Container $container): void
+    {
+        $serviceProviders = $this->getServiceProviders();
+        if (! in_array(WordpressProvider::class, $serviceProviders, true)) {
+            $serviceProviders[WordpressProvider::class] = [];
+        }
+
+        /**
+         * These service providers should add dependencies and methods that need to be globally available,
+         * they should not be hooked directly to WordPress' actions or filters.
+         */
+        foreach ($serviceProviders as $provider => $arguments) {
+            if (! is_subclass_of($provider, ServiceProviderInterface::class)) {
+                throw new DoesNotImplementProviderInterfaceException($provider);
+            }
+
+            if ($provider === WordpressProvider::class) {
+                $container->register(new $provider($this->getPluginName(), $this->getPluginFile()), $arguments);
+                continue;
+            }
+
+            $container->register(new $provider(), $arguments);
+        }
     }
 }
