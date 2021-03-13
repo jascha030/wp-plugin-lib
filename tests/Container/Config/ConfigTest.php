4b61b5fc8452d5f4df38e912543fea6c70e62313
Jascha van Aalst <mr.jascha@gmail.com>
2021-03-13 04:55:21 +0100

    Added and passing ConfigTest

--- tests/Container/Config/ConfigTest.php
+++ tests/Container/Config/ConfigTest.php
@@ -3,12 +3,34 @@
 namespace Jascha030\Tests\Container\Config;
 
 use Jascha030\PluginLib\Container\Config\Config;
+use Jascha030\PluginLib\Container\Config\ConfigInterface;
 use PHPUnit\Framework\TestCase;
 
 class ConfigTest extends TestCase
 {
-    final public function testConstruct(): void
+    public const TEST_PLUGIN_NAME = 'TestPlugin';
+
+    final public function testConstruct(): ConfigInterface
     {
-        $config = new Config('TestPlugin', __FILE__);
+        $config = new Config(self::TEST_PLUGIN_NAME, __FILE__);
+
+        self::assertInstanceOf(ConfigInterface::class, $config);
+
+        return $config;
+    }
+
+    /**
+     * @depends testConstruct
+     *
+     * @param  ConfigInterface  $config
+     */
+    final public function testAccessMethods(ConfigInterface $config): void
+    {
+        self::assertEquals(self::TEST_PLUGIN_NAME, $config->getPluginName());
+        self::assertEquals(__FILE__, $config->getPluginFile());
+
+        self::assertEquals([], $config->getHookables());
+        self::assertEquals([], $config->getServiceProviders());
+        self::assertEquals([], $config->getPostTypes());
     }
 }
