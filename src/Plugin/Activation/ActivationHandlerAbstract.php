e1d553234116a6718096c399b62699e8f7ea1952
Jascha van Aalst <mr.jascha@gmail.com>
2021-04-05 02:16:24 +0200

    Method to add activation filters

--- src/Plugin/Activation/ActivationHandlerAbstract.php
+++ src/Plugin/Activation/ActivationHandlerAbstract.php
@@ -2,6 +2,12 @@
 
 namespace Jascha030\PluginLib\Plugin\Activation;
 
+use Closure;
+
+/**
+ * Class ActivationHandlerAbstract
+ * @package Jascha030\PluginLib\Plugin\Activation
+ */
 abstract class ActivationHandlerAbstract implements OnActivateInterface
 {
     private string $pluginFilePath;
@@ -11,14 +17,39 @@ abstract class ActivationHandlerAbstract implements OnActivateInterface
         $this->pluginFilePath = $pluginFilePath;
     }
 
-    public static function activation(string $pluginFilePath, string $pluginPrefix, array $config = []): void
+    /**
+     * Create an instance and apply filters added through the `static::add()` method
+     *
+     * @param string $pluginFilePath
+     * @param array  $config
+     */
+    public static function activation(string $pluginFilePath, array $config = []): void
     {
         $activation = new static($pluginFilePath);
-        $activation = apply_filters($pluginPrefix . '_activation_handlers', $activation);
+        $activation = apply_filters(self::sanitizePluginPath($pluginFilePath) . '_activation_handlers', $activation);
 
         $activation->register();
     }
 
+    /**
+     * Add callbacks on activation
+     *
+     * @param string  $pluginFilePath
+     * @param Closure $closure
+     */
+    final public static function add(string $pluginFilePath, Closure $closure): void
+    {
+        add_filter(self::sanitizePluginPath($pluginFilePath) . '_activation_handlers', $closure);
+    }
+
+    private static function sanitizePluginPath(string $pluginFilePath): string
+    {
+        return str_replace('/', '_', $pluginFilePath);
+    }
+
+    /**
+     * @inheritDoc
+     */
     final public function register(): void
     {
         \register_activation_hook($this->pluginFilePath, [$this, 'activate']);
