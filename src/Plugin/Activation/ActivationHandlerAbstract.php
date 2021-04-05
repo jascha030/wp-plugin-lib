7a22da16c96b1ac841a125da09a99397eace5c4a
Jascha van Aalst <mr.jascha@gmail.com>
2021-04-05 02:50:11 +0200

    Move methods to generic activation class instead of abstract

--- src/Plugin/Activation/ActivationHandlerAbstract.php
+++ src/Plugin/Activation/ActivationHandlerAbstract.php
@@ -2,8 +2,6 @@
 
 namespace Jascha030\PluginLib\Plugin\Activation;
 
-use Closure;
-
 /**
  * Class ActivationHandlerAbstract
  * @package Jascha030\PluginLib\Plugin\Activation
@@ -18,40 +16,15 @@ abstract class ActivationHandlerAbstract implements OnActivateInterface
     }
 
     /**
-     * Create an instance and apply filters added through the `static::add()` method
-     *
-     * @param string $pluginFilePath
-     * @param array  $config
-     */
-    public static function activation(string $pluginFilePath, array $config = []): void
-    {
-        $activation = new static($pluginFilePath);
-        $activation = apply_filters(self::sanitizePluginPath($pluginFilePath) . '_activation_handlers', $activation);
-
-        $activation->register();
-    }
-
-    /**
-     * Add callbacks on activation
-     *
-     * @param string  $pluginFilePath
-     * @param Closure $closure
-     */
-    final public static function add(string $pluginFilePath, Closure $closure): void
-    {
-        add_filter(self::sanitizePluginPath($pluginFilePath) . '_activation_handlers', $closure);
-    }
-
-    private static function sanitizePluginPath(string $pluginFilePath): string
-    {
-        return str_replace('/', '_', $pluginFilePath);
-    }
-
-    /**
      * @inheritDoc
      */
     final public function register(): void
     {
         \register_activation_hook($this->pluginFilePath, [$this, 'activate']);
+    }
+
+    final protected function getPluginFilePath(): string
+    {
+        return $this->pluginFilePath;
     }
 }
